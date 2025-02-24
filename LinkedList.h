eb091e91be8495a1db81f77951df79a54504a45a
rislynraja <rislyn_ferona.raja@uconn.edu>
2025-02-24 04:04:20 +0000

    first commit

---
+++ LinkedList.h
@@ -0,0 +1,36 @@
+#ifndef _LINKED_LIST_H
+#define _LINKED_LIST_H
+
+#include <iostream>
+#include "Node.h"
+
+using namespace std;
+
+class LinkedList {
+  public:
+
+    Node * root;
+
+    LinkedList(); // constructor
+    LinkedList(const LinkedList& copy); // copy constructor
+    ~LinkedList(); // destructor
+
+    // given functions (defined)
+    void insertAtBeginning(int val);
+    void insertAtEnd(int val);
+    Node * getLastElement();
+    void deleteNode(int val);
+    Node * build_linked_list(int nodes) ;
+    void print_linked_list() const;
+
+    // functions that need to be written
+    void delete_entire_linked_list();
+    int get_linked_list_data_item_value(int node_number) const;
+    void pointerJumping();
+    bool hasNegativePrefixSum() const;
+    bool hasPositivePrefixSum() const;
+    bool delete_list_element(int node_number);
+
+};
+
+#endif
